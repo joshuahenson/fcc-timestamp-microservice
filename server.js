042169fe543110c93a19926a22cdaba8ac3db4a3
Joshua Henson <joshuahenson@users.noreply.github.com>
2015-12-31 22:50:33 -0800

    update port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -8,26 +10,25 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:input', function (req, res) {
-    var param = req.params.input;
-    var month = [ "January ", "February ", "March ", "April ",
-                  "May ", "June ","July ", "August ", "September ",
-                  "October ", "November ", "December " ];
-    if (isNaN(param)) {
-      var date = new Date(param);
-      if (date == 'Invalid Date') {
-        return res.send({"unix": null, "natural": null})
-      }
-    } else {
-      date = new Date(param*1000);
+  var param = req.params.input;
+  var month = [ "January ", "February ", "March ", "April ",
+                "May ", "June ","July ", "August ", "September ",
+                "October ", "November ", "December " ];
+  if (isNaN(param)) {
+    var date = new Date(param);
+    if (date == 'Invalid Date') {
+      return res.send({"unix": null, "natural": null})
     }
-    var jsonResponse = {
-      "unix": Math.floor(date.getTime() / 1000) ,//UTC
-      "natural": month[date.getUTCMonth()] + date.getUTCDate() + ', ' + date.getUTCFullYear()
-    }
-    res.send(jsonResponse);
-  });
+  } else {
+    date = new Date(param*1000);
+  }
+  var jsonResponse = {
+    "unix": Math.floor(date.getTime() / 1000) ,//UTC
+    "natural": month[date.getUTCMonth()] + date.getUTCDate() + ', ' + date.getUTCFullYear()
+  }
+  res.send(jsonResponse);
+});
 
-var port = 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
